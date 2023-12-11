8ee0e8956be2d56192b8f3786b7f04908e41b3a5
Elisa Klunder <elisaklunder@hotmail.com>
2023-12-11 12:03:08 +0100

    second test fails

--- part_1/src/tests/test_codemaker.py
+++ part_1/src/tests/test_codemaker.py
@@ -9,8 +9,14 @@ class Tests(unittest.TestCase):
 
     # fail: code is nothing, pass: otherwise
     def test_there_is_code(self):
-        self.coder = Codemaker()
-        self.assertIsNotNone(self.coder._make_code())
+        coder = Codemaker()
+        self.assertIsNotNone(coder._make_code())
+
+    # fail: code is not a set of strings, pass: otherwise
+    def test_code_is_strings(self):
+        coder = Codemaker()
+        code = coder._make_code()
+        self.assertTrue(all(isinstance(element, str) for element in code))
 
 
 if __name__ == '__main__':
