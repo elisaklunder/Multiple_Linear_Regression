6521d120fdf8e94016fdb77af413ff959fac87e9
Julia Belloni <juliabelloni@RN-145-90-160-128.eduroam.rug.nl>
2023-12-11 10:02:13 +0100

    starting code

---
+++ part_1/codebreaker.py
@@ -0,0 +1,3 @@
+
+class Codebreaker:
+    pass
---
+++ part_1/src/tests/tests.py
@@ -0,0 +1,8 @@
+import unittest
+from part_1 import game, codebreaker, codemaker
+
+class Tests(unittest.TestCase):
+
+    def test_there_is_4_symbol_code(self):
+        pass
+    pass
