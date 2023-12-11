5218ef1592158324a503e32627154f982c027710
Elisa Klunder <elisaklunder@hotmail.com>
2023-12-11 10:47:20 +0100

    sfk

--- part_1/src/tests/tests.py
+++ part_1/src/tests/tests.py
@@ -2,13 +2,14 @@ import unittest
 import sys
 import os
 sys.path.append(os.getcwd() + "/part_1/src/")
-import codemaker
+from codemaker import Codemaker
 
 
 class Tests(unittest.TestCase):
 
     def test_there_is_code(self):
-        self.assertIsNotNone(codemaker.make_code())
+        coder = Codemaker()
+        self.assertIsNotNone(coder.make_code())
 
         pass
     pass
