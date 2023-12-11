66ca237afdcf710ec4e00b9bad2b32d45df612d7
Elisa Klunder <elisaklunder@hotmail.com>
2023-12-11 12:29:34 +0100

    fourth test fails

--- part_1/src/codemaker.py
+++ part_1/src/codemaker.py
@@ -4,9 +4,15 @@ import random
 class Codemaker:
     def __init__(self) -> None:
         self._code = []
-        self.guess = []
         self._colors = ["W", "K", "Y", "G", "R", "B"]
 
     def _make_code(self):
         self._code = random.choices(self._colors, k=4)
         return self._code
+
+    # assume for now that guess is a valid guess
+    def _correct_guesses_right_pos(self, guess):
+        pass
+
+    def _correct_guesses_wrong_pos(self, guess):
+        pass
--- part_1/src/tests/test_codemaker.py
+++ part_1/src/tests/test_codemaker.py
@@ -7,22 +7,28 @@ from codemaker import Codemaker
 
 class Tests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(self):
+        self.coder = Codemaker()
+
     # fail: code is nothing, pass: otherwise
     def test_there_is_code(self):
-        coder = Codemaker()
-        self.assertIsNotNone(coder._make_code())
+        self.assertIsNotNone(self.coder._make_code())
 
     # fail: code is not a set of strings, pass: otherwise
     def test_code_is_strings(self):
-        coder = Codemaker()
-        code = coder._make_code()
+        code = self.coder._make_code()
         self.assertTrue(all(isinstance(element, str) for element in code))
-    
+
     # fail: code doesn't have len = 4, pass: otherwise 
     def test_code_has_4_elements(self):
-        coder = Codemaker()
-        code = coder._make_code()
+        code = self.coder._make_code()
         self.assertTrue(len(code) == 4)
+
+    def test_correct_guesses_right_pos_is_number(self):
+        guess = ["B", "B", "Y", "R"]
+        self.assertIsInstance(self.coder._correct_guesses_right_pos(guess),
+                              int)
 
 
 if __name__ == '__main__':
