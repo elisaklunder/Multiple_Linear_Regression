6baa846a601d5f43a36d07111a8ff6b325b0ca4a
Elisa Klunder <elisaklunder@hotmail.com>
2023-12-18 12:31:06 +0100

    main with data from not sklearn

--- part_2/main.py
+++ part_2/main.py
@@ -1,17 +1,20 @@
 import os
 import sys
 
+import numpy as np
+
 sys.path.append(os.getcwd() + "/part_2/src")
-from sklearn import datasets, linear_model
-from src.multiple_linear_regression import MultipleLinearRegression
 from src.gd_multiple_linear_regression import GDMultipleLinearRegression
 from src.lasso_regression import LassoRegression
 from src.loss_function import LossFunction
 from src.model_saver import ModelSaver
+from src.multiple_linear_regression import MultipleLinearRegression
 from src.ridge_regression import RidgeRegression
 
 if __name__ == "__main__":
-    X, y = datasets.load_diabetes(return_X_y=True)
+    # Load data
+    X = np.load("X.npy")
+    y = np.load("y.npy")
 
     # Split the data into training/testing sets
     X_train = X[:-20]
@@ -21,56 +24,30 @@ if __name__ == "__main__":
     y_train = y[:-20]
     y_test = y[-20:]
 
-    # Our Lasso vs sklearn Lasso
-    our_lasso = LassoRegression()
-    our_lasso.train(X_train, y_train)
-    our_lasso_predictions = our_lasso.predict(X_test)
+    # Lasso
+    lasso = LassoRegression()
+    lasso.train(X_train, y_train)
+    lasso_predictions = lasso.predict(X_test)
+    print(f"Lasso predictions are:\n {lasso_predictions}")
 
-    sklearn_lasso = linear_model.Lasso()
-    sklearn_lasso.fit(X_train, y_train)
-    sklearn_lasso_predictions = sklearn_lasso.predict(X_test)
+    # Ridge
+    ridge = RidgeRegression()
+    ridge.train(X_train, y_train)
+    ridge_predictions = ridge.predict(X_test)
+    print(f"Ridge predictions are:\n {ridge_predictions}")
 
-    # Compare lasso predictions
-    print(f"Our lasso predictions are:\n {our_lasso_predictions}")
-    print(f"Sklearn lasso predictions are:\n {sklearn_lasso_predictions}\n")
+    # Gradient Descent Multiple Linear Regression
+    gd_mlr = GDMultipleLinearRegression()
+    gd_mlr.train(X_train, y_train)
+    gd_mlr_predictions = gd_mlr.predict(X_test)
+    print(
+        f"GD Multiple Linear Regression predictions are:\n\
+{gd_mlr_predictions}"
+    )
 
-    # Our ridge vs sklearn ridge
-    our_ridge = RidgeRegression()
-    our_ridge.train(X_train, y_train)
-    our_ridge_predictions = our_ridge.predict(X_test)
-
-    sklearn_ridge = linear_model.Ridge()
-    sklearn_ridge.fit(X_train, y_train)
-    sklearn_ridge_predictions = sklearn_ridge.predict(X_test)
-
-    # Compare ridge predictions
-    print(f"Our ridge predictions are:\n {our_ridge_predictions}")
-    print(f"Sklearn ridge predictions are:\n {sklearn_ridge_predictions}\n")
-
-    # Our GD linear regression vs sklearn one
-    our_gd_mlr = GDMultipleLinearRegression()
-    our_gd_mlr.train(X_train, y_train)
-    our_gd_mlr_predictions = our_gd_mlr.predict(X_test)
-
-    sklearn_gd_mlr = linear_model.SGDRegressor()
-    loss = LossFunction()
-    sklearn_gd_mlr.fit(X_train, y_train)
-    sklearn_gd_mlr_predictions = sklearn_gd_mlr.predict(X_test)
-
-    sk_mae = loss.mean_absolute_error(sklearn_gd_mlr_predictions, y_test)
-    sk_mse = loss.mean_squared_error(sklearn_gd_mlr_predictions, y_test)
-    our_mae = loss.mean_absolute_error(our_gd_mlr_predictions, y_test)
-    our_mse = loss.mean_squared_error(our_gd_mlr_predictions, y_test)
-
-    # Compare ridge predictions
-    print(f"Our gd_mlr predictions are:\n {our_gd_mlr_predictions}")
-    print(f"Sklearn gd_mlr predictions are:\n {sklearn_gd_mlr_predictions}\n")
-
-    print(our_mae, our_mse)
-    print(sk_mae, sk_mse)
-    # save and load weights
+    # Save and load weights
     saver = ModelSaver()
-    saver.save_weights(our_ridge, "./weights_ridge.csv")
+    saver.save_weights(ridge, "./weights_ridge.csv")
     mlr = MultipleLinearRegression()
     saver.load_weights(mlr, "./weights_ridge.csv")
     print(f"Predictions with loaded model:\n {mlr.predict(X_test)}")
